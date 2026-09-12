5756dbc19fd9a55751d441237836a4cf88d83fb8
时运Ⅲ <z3093508903@gmail.com>
2026-09-12 16:00:09 +0800

    fix: use exact permission callback signature

--- app/src/main/java/io/github/messagerelay/EchoLauncherActivity.kt
+++ app/src/main/java/io/github/messagerelay/EchoLauncherActivity.kt
@@ -26,7 +26,7 @@ class EchoLauncherActivity : ComponentActivity() {
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
